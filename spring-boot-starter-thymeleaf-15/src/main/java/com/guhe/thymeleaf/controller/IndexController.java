6221b1bf4fc064101a1e64d96217b6e36f87f89f
Stein283036 <597061520@qq.com>
2023-03-15 20:48:08 +0800

    15-Spring Boot 使用 echarts 绘制可视化图表。

--- spring-boot-starter-thymeleaf-15/src/main/java/com/guhe/thymeleaf/controller/IndexController.java
+++ spring-boot-starter-thymeleaf-15/src/main/java/com/guhe/thymeleaf/controller/IndexController.java
@@ -3,11 +3,13 @@ package com.guhe.thymeleaf.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * @author njl
  * @date 2023/3/15
  */
+@RequestMapping("/thymeleaf")
 @Controller
 public class IndexController {
 	@GetMapping
@@ -15,5 +17,10 @@ public class IndexController {
 		modelMap.put("username", "stein");
 		modelMap.put("password", "njl283036");
 		return "index";
+	}
+
+	@GetMapping("/echarts")
+	public String echarts() {
+		return "echarts";
 	}
 }
