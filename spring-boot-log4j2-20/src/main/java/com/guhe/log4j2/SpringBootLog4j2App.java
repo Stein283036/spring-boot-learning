4c289d5a2ebf371abc5c7c07e6a9d6d4e1d02823
Stein283036 <597061520@qq.com>
2023-03-16 20:54:15 +0800

    20-在 Spring Boot 中使用 log4j2 记录系统日志，详细文档如具体配置可见官网 https://logging.apache.org/log4j/2.x/。

--- spring-boot-log4j2-20/src/main/java/com/guhe/log4j2/SpringBootLog4j2App.java
+++ spring-boot-log4j2-20/src/main/java/com/guhe/log4j2/SpringBootLog4j2App.java
@@ -1,11 +1,20 @@
 package com.guhe.log4j2;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @author njl
  * @date 2023/3/16
  */
+@Slf4j
+@SpringBootApplication
 public class SpringBootLog4j2App {
 	public static void main(String[] args) {
-
+		SpringApplication.run(SpringBootLog4j2App.class, args);
+		log.info("Logger name - {}", log.getName());
+		log.info("info 日志");
+		log.error("error 日志");
 	}
 }
